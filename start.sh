ccce9f7827da5d88b04d6a27cddb2ad4f81bd13b
sarhad <71734492+SarhadCodes@users.noreply.github.com>
2025-12-17 03:06:21 +0300

    Create PocketBase superuser

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 chmod +x pocketbase
-./pocketbase serve --http=0.0.0.0:$PORT
+./pocketbase serve \
+  --http=0.0.0.0:10000 \
+  --admin-email=admin@iptv.local \
+  --admin-password=Admin12345
+
 
