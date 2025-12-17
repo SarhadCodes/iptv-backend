b5a7b3f460a10b62bde7088e25a4c632da4b9bd0
sarhad <71734492+SarhadCodes@users.noreply.github.com>
2025-12-17 03:11:31 +0300

    Persist PocketBase admin via pb_data

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 chmod +x pocketbase
-./pocketbase serve \
-  --http=0.0.0.0:10000 \
-  --admin-email=admin@iptv.local \
-  --admin-password=Admin12345
+./pocketbase serve --http=0.0.0.0:10000
 
 
